4ed73d7cbd0a9101e7f3ab99ebdf3ba904b09d10
NEVA <neva.vka@yandex.ru>
2022-10-07 21:57:28 +0300

    Третье задание

---
+++ Dz03/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Clear();
+Console.Write("Введите число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+if (a == 6 || a == 7) 
+{
+    Console.Write("Выходной");
+}
+else if (a > 7 || a <=0)
+{
+    Console.Write("Не существует");
+}
+else Console.Write("Рабочий день");
