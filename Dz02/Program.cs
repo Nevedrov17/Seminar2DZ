6f851c0302ec499544badb199f93389ba935ce74
NEVA <neva.vka@yandex.ru>
2022-10-07 21:33:31 +0300

    Второе задание

---
+++ Dz02/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Clear();
+Console.Write("Введите число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+if(a > 99)
+{
+    while(a > 999)
+    {
+       a = a/10;
+    }
+     Console.WriteLine( $"Третье число {a%10} ");
+}
+else Console.WriteLine("Третьей цифры нет");
